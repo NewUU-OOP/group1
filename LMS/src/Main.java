dec0d2f3e75466d642296e75fc5c6e22baa04384
macbookair <s.djuraev@newuzbekistanuniversity.uz>
2023-12-06 14:00:02 +0500

    V1

--- LMS/src/Main.java
+++ LMS/src/Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Welcome to Library Management System");
+        Address univerAddress = new Address("Movarounnahr 1","Tashkent","Tashkent","122121","Uzbekistan");
+        Library library = new Library( "NUU Library", univerAddress);
+        System.out.println(library.toString());
+
+
+
     }
 }
