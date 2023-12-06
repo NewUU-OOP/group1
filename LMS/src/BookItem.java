bb7e5031d178276f6011c9ba43107ba5de9b73a3
macbookair <s.djuraev@newuzbekistanuniversity.uz>
2023-12-06 14:45:47 +0500

    V1

--- LMS/src/BookItem.java
+++ LMS/src/BookItem.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+import java.util.List;
 
 public class BookItem extends Book {
 
@@ -12,6 +13,45 @@ public class BookItem extends Book {
     private Date dateOfPurchase;
     private Date publicationDate;
     private Rack placedAt;
+
+    public BookItem(String ISBN, String title, String subject, String publisher,
+                    String language, int numberOfPages, List<Author> authors,
+                    String barcode, boolean isReferenceOnly, Date borrowed, Date dueDate,
+                    double price, BookFormat format, BookStatus status, Date dateOfPurchase,
+                    Date publicationDate, Rack placedAt) {
+        super(ISBN, title, subject, publisher, language, numberOfPages, authors);
+        this.barcode = barcode;
+        this.isReferenceOnly = isReferenceOnly;
+        this.borrowed = borrowed;
+        this.dueDate = dueDate;
+        this.price = price;
+        this.format = format;
+        this.status = status;
+        this.dateOfPurchase = dateOfPurchase;
+        this.publicationDate = publicationDate;
+        this.placedAt = placedAt;
+    }
+
+    public BookItem(String ISBN, String title, String subject, String publisher, String language, int numberOfPages, List<Author> authors) {
+        super(ISBN, title, subject, publisher, language, numberOfPages, authors);
+
+    }
+
+    @Override
+    public String toString() {
+        return "BookItem{" +
+                "barcode='" + barcode + '\'' +
+                ", isReferenceOnly=" + isReferenceOnly +
+                ", borrowed=" + borrowed +
+                ", dueDate=" + dueDate +
+                ", price=" + price +
+                ", format=" + format +
+                ", status=" + status +
+                ", dateOfPurchase=" + dateOfPurchase +
+                ", publicationDate=" + publicationDate +
+                ", placedAt=" + placedAt +
+                '}';
+    }
 
     public boolean checkout(String memberId) {
 
--- LMS/src/Main.java
+++ LMS/src/Main.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -5,6 +9,17 @@ public class Main {
         Address univerAddress = new Address("Movarounnahr 1","Tashkent","Tashkent","122121","Uzbekistan");
         Library library = new Library( "NUU Library", univerAddress);
         System.out.println(library.toString());
+        Author author1 = new Author("Jack London"," Jack London  writer. Marti Eden");
+        Author author2 = new Author("Hudoyberdi Tuxtaboyev","He wrote many books about Hoshimjon and his friend.");
+        List<Author> authorList = new ArrayList<>();
+        authorList.add(author2);
+        BookItem bookItem1 = new BookItem("47878839788","Sariq Devni Minib","Subject",
+                "Yangi Avlod","Uzbek",378, authorList,"343434",false,new Date("2023/10/23"),
+                new Date("2023/10/39"),35000, BookFormat.HARDCOVER,BookStatus.AVAILABLE,
+                new Date("2022/1/1"), new Date("1987/2/10"),new Rack());
+
+        System.out.println(bookItem1.toString());
+
 
 
 
