bf132dde994c88fbbfcbc29871bd137a560fc874
macbookair <s.djuraev@newuzbekistanuniversity.uz>
2023-12-13 13:29:05 +0500

    V1

---
+++ LMSA/src/main/java/org/example/MainPage.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainPage extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+            //New comments
+
+            BorderPane borderPane = new BorderPane();
+            Scene scene = new Scene(borderPane,400,450);
+            primaryStage.setTitle("Library Management System");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+}
